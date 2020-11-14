d38a98eea8347647353a234cac37125b07217ba2
YelloowBlitz <lopite2@live.fr>
2020-11-14 17:00:36 +0100

    Correction du path

--- Assets/Scripts/TilemapPath.cs
+++ Assets/Scripts/TilemapPath.cs
@@ -4,7 +4,58 @@ using UnityEngine;
 
 public class TilemapPath : MonoBehaviour
 {
-    public List<Vector2> path1;
+    public List<Vector2> path1 = new List<Vector2>();
+
+    void Start()
+    {
+        if (path1.Count == 0)
+        {
+            path1.Add(new Vector2(0, 8));
+            path1.Add(new Vector2(1, 8));
+            path1.Add(new Vector2(2, 8));
+            path1.Add(new Vector2(3, 8));
+            path1.Add(new Vector2(4, 8));
+            path1.Add(new Vector2(5, 8));
+            path1.Add(new Vector2(6, 8));
+            path1.Add(new Vector2(6, 9));
+            path1.Add(new Vector2(6, 10));
+            path1.Add(new Vector2(6, 11));
+            path1.Add(new Vector2(6, 12));
+            path1.Add(new Vector2(7, 12));
+            path1.Add(new Vector2(8, 12));
+            path1.Add(new Vector2(9, 12));
+            path1.Add(new Vector2(10, 12));
+            path1.Add(new Vector2(11, 12));
+            path1.Add(new Vector2(12, 12));
+            path1.Add(new Vector2(12, 11));
+            path1.Add(new Vector2(12, 10));
+            path1.Add(new Vector2(12, 9));
+            path1.Add(new Vector2(12, 8));
+            path1.Add(new Vector2(13, 8));
+            path1.Add(new Vector2(14, 8));
+            path1.Add(new Vector2(15, 8));
+            path1.Add(new Vector2(16, 8));
+            path1.Add(new Vector2(17, 8));
+            path1.Add(new Vector2(18, 8));
+            path1.Add(new Vector2(19, 8));
+            path1.Add(new Vector2(20, 8));
+            path1.Add(new Vector2(21, 8));
+            path1.Add(new Vector2(22, 8));
+            path1.Add(new Vector2(23, 8));
+            path1.Add(new Vector2(23, 7));
+            path1.Add(new Vector2(23, 6));
+            path1.Add(new Vector2(23, 5));
+            path1.Add(new Vector2(23, 4));
+            path1.Add(new Vector2(23, 3));
+            path1.Add(new Vector2(23, 2));
+            path1.Add(new Vector2(22, 2));
+            path1.Add(new Vector2(21, 2));
+            path1.Add(new Vector2(20, 2));
+            path1.Add(new Vector2(19, 2));
+            path1.Add(new Vector2(18, 2));
+            path1.Add(new Vector2(17, 2));
+        }
+    }
 
     void OnDrawGizmosSelected() // Affiche des ronds sur le chemins
     {
