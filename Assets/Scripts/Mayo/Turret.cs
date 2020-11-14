55b436afddae7210c5d1ea23a391474a24e27298
Vivien GAGLIANO <gagliano.vivien@gmail.com>
2020-11-14 02:35:09 +0100

    Added turret abstract class and interface

---
+++ Assets/Scripts/Mayo/Turret.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Turret : MonoBehaviour
+{
+    [SerializeField] private int attackRate;
+    [SerializeField] private float attackDamage;
+    [Space]
+    [SerializeField] private int buildCost;
+    [SerializeField] private int rankOneCost;
+    [SerializeField] private int rankTwoCost;
+
+    private int rank = 0;
+}
