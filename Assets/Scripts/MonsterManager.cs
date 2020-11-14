9a2caed79b7598200b92213c22dd3e249ca9f89e
Vivien GAGLIANO <gagliano.vivien@gmail.com>
2020-11-14 13:29:03 +0100

    Fixed minor stuff

--- Assets/Scripts/MonsterManager.cs
+++ Assets/Scripts/MonsterManager.cs
@@ -19,8 +19,8 @@ public class MonsterManager : MonoBehaviour
 
     public Text enemySoulsText;
     public Text friendlySoulsText;
-    public int enemySouls = 0;
-    public int friendlySouls = 0;
+    private int enemySouls = 0;
+    private int friendlySouls = 0;
 
     void Start()
     {
